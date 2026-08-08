eb1edaf8b98a20df9d3e93cc7870c606dc2e1668
bzhasso019 <bzhassokubsu@gmail.com>
2026-08-08 17:22:31 +0300

    The project beginning

---
+++ src/test/java/my/shapsug/eventProject/EventProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package my.shapsug.eventProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
